a01c180bfd7db8a2d1a0b15138eba6556b427e43
Alaa Fayed <alaafayed810@gmail.com>
2024-09-05 18:39:43 +0300

    0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,4 +1,10 @@
+#!/usr/bin/python3
+"""python"""
+
+
 def validUTF8(data):
+    """method that determines if a given data set represents a
+    valid UTF-8 encoding"""
     # Number of bytes in the current UTF-8 character
     num_bytes = 0
 
